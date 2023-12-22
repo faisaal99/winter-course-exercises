7dabbab0ccd54db02293bdcc5168e1c2a0152a15
Faisal A. <akhtarfaisal147@gmail.com>
2023-12-22 08:20:12 +0100

    work in ex1.py for t1

--- t1/variables/ex1.py
+++ t1/variables/ex1.py
@@ -1,3 +1,4 @@
+
 #                            Variabler - Övning 1
 #
 # Denna uppgift handlar om att ändra på några detaljer i en kort berättelse om
@@ -5,10 +6,12 @@
 # hjälper programmeraren att hålla reda på data och information.
 #
 
-# Dessa nedan kallas för "variabler". De används till att spara data i en
+# Dessa nedan kallas för "variabler". De används till att spara data, och att
+# sedan återanvända det.
 name = "Adbi"
 age = 15
 
+# Detta är en kort berättelse om 
 story = f"""\
 Det här är en berättelse om min vän {name}.
 Han är {age} år gammal och kommer från en by inte långt härifrån.
@@ -18,10 +21,25 @@ Men {name} har ett problem, han glömde plånboken på tåget.
 Han behöver ta sig tillbaka till tågstationen och förklara för någon att han
 tappat bort viktiga ägodelar på tåget.
 
-Om ett par månader blir Abdi {age + 1} år gammal.\
+Om ett par månader blir Abdi {age + 1} år gammal.
 """
 
 
 print(story)
 
+is_done = False
+
+def do_once(func) -> bool:
+    global is_done
+
+    if not is_done:
+        func()
+        is_done = True
+        return True
+    else:
+        return False
+
 ################################################################################
+
+while True:
+    do_once(lambda: print('Moustafa sucks'))
